6ae5d7f756275d8c27607b07d93f1e4f0ec9015e
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2024-01-03 18:19:32 +0700

    feat: add solve

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """
 
 ## input
-year_born = input("Enter your year of birth in AD :")
+year_born = int(input("Enter your year of birth in AD :"))
 
 ## print year after cal
+print("In 10 years you will be",(2024-year_born)+10,"years old.")
